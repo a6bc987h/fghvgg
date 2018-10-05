43be9d0962198472a13790ad66b9558f081f732a
a6bc987h <43772681+a6bc987h@users.noreply.github.com>
2018-10-05 14:29:41 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -21,14 +21,11 @@ PM_START_TEXT = """
 
 Hai {}, 
 My name is {}! 
-I'm a group manager bot maintained by [this noob](t.me/TonyJr_Online).
-I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
-For all your doubts about telegram use [TG_Helpbot](https://t.me/tg_helpbot) ..
-Using standard [Marie](http://t.me/BanhammerMarie_bot) is always gonna be easier.
-You can see my updates [here](https://t.me/TonyJr_Online).
+I'm a group manager bot of [ɴᴇᴡs ᴄᴏʀɴᴇʀ](t.me/news_corner).
+I'm here to help you manage ɴᴇᴡs ᴄᴏʀɴᴇʀ! Hit /help to find out more about how to use me to my full potential.
+Our Channel 👉 @TgNewsCorner
 
-
-[Click here to add me in your groups](https://telegram.me/tonyjr_bot?startgroup=add)
+[Click Here To Join Our Group](https://telegram.me/news_corner)
 """
 
 HELP_STRINGS = """
@@ -38,16 +35,13 @@ I'm a modular group management bot with a few fun extras! Have a look at the fol
 A few handy commands are:
  - /start: starts me! You've probably already used this.
  - /help: gets this message out; I'll tell you more about myself!
- - /donate: gives you info on how to support me and my creator.
+ - /about: get more details about us.
 
 
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/tonyjr_online) to get me to where I am now, and every donation helps \
-motivate him to make me even better. 
-[Click here](https://telegra.ph/Donate-10-04) for donate me .... Thank you"""
+ABOUT_STRING = """[Click here to know more about us](https://telegra.ph/GROUP-RULES-08-12)"""
 
 IMPORTED = {}
 MIGRATEABLE = []
